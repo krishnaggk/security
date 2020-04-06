0b4409815ef75dba939fa8a419cb7ca14233b478
Gopala Krishna Ambareesh <gopalak@amazon.com>
2020-04-06 18:13:02 +0530

    Implement APIs and datamodel to configure nodes_dn dynamically.
    The primary usecase is targeted at cross-cluster where in nodes restart
    can be avoided by populating the coordinating cluster's nodes_dn values
    to facilitate cross-cluster usecases.
    
    **Summary of changes**:
    
    - Introduce "nodesdn" datatype to be stored in security index. In a way
      it is another permission entity similar to internaluser, role etc
    - Make the above datatype backward compatible i.e do not fail other
      operations (roles, users etc)
    - Implement APIs for CRUD operations that are only super-admin
      accessible and are only enabled based on a YML config value.

--- src/main/java/com/amazon/opendistroforelasticsearch/security/configuration/OpenDistroSecurityIndexSearcherWrapper.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/security/configuration/OpenDistroSecurityIndexSearcherWrapper.java
@@ -39,6 +39,7 @@ import com.amazon.opendistroforelasticsearch.security.securityconf.ConfigModel;
 import com.amazon.opendistroforelasticsearch.security.securityconf.DynamicConfigFactory;
 import com.amazon.opendistroforelasticsearch.security.securityconf.DynamicConfigModel;
 import com.amazon.opendistroforelasticsearch.security.securityconf.InternalUsersModel;
+import com.amazon.opendistroforelasticsearch.security.securityconf.NodesDnModel;
 import com.amazon.opendistroforelasticsearch.security.support.WildcardMatcher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -82,7 +83,7 @@ public class OpenDistroSecurityIndexSearcherWrapper extends IndexSearcherWrapper
     }
 
     @Override
-    public void onChanged(ConfigModel cm, DynamicConfigModel dcm, InternalUsersModel ium) {
+    public void onChanged(ConfigModel cm, DynamicConfigModel dcm, InternalUsersModel ium, NodesDnModel nm) {
         this.configModel = cm;
     }
 
